fdc8a6754d3569e119e5ca4c2c0749d813826264
petar <petar@localhost>
2009-05-12 15:39:45 +0200

    Cache should be written to disk as soon as possible to minimize data loss

--- metaTED/cache.py
+++ metaTED/cache.py
@@ -5,7 +5,8 @@ from shove.store.file import FileStore
 
 cached_storage = Shove(
     store=FileStore(os.path.expanduser('~/.metaTED/cache')),
-    cache='simplelru://'
+    cache='simplelru://',
+    sync=1 # Minimizes data loss on various processing errors
 )
 
 cache = cached_storage._cache
