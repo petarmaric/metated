72ac5c1d8f9604dff0e1b1e90b6ef19a7154f372
Petar Marić <petar.maric@gmail.com>
2011-11-06 01:03:05 +0100

    Switched from `urllib2` to `requests`, removed page caching as it's never used

--- metaTED/crawler/__init__.py
+++ metaTED/crawler/__init__.py
@@ -1,24 +1,12 @@
 import logging
-import urllib2
 import metaTED
-from metaTED.cache import cache
+import requests
 
 
 _DEFAULT_RETRY_TIMES = 5
 
 
-_opener = urllib2.build_opener()
-_opener.addheaders = [('User-agent', 'metaTED/%s' % metaTED.__version__)]
-
-
 def urlread(fullurl, max_retries=_DEFAULT_RETRY_TIMES):
-    # Check in-memory cache before requesting url
-    logging.debug("Searching cache for '%s' contents...", fullurl)
-    if fullurl in cache:
-        logging.debug("Found the cached version of '%s' contents", fullurl)
-        return cache[fullurl]
-    logging.debug("Failed to find the cached version of '%s' contents", fullurl)
-
     saved_exception = None
     for try_num in xrange(1, max_retries+1):
         try:
@@ -28,11 +16,18 @@ def urlread(fullurl, max_retries=_DEFAULT_RETRY_TIMES):
                 try_num,
                 max_retries
             )
-            data = _opener.open(fullurl).read()
+            r = requests.get(
+                url=fullurl,
+                headers={
+                    'User-Agent': "metaTED/%s" % metaTED.__version__,
+            })
+            
+            # Check if we made a bad request
+            r.raise_for_status()
+            
             logging.debug("Successfully read data from '%s'", fullurl)
-            cache[fullurl] = data
-            return data
-        except urllib2.URLError, e:
+            return r.content
+        except requests.RequestException, e:
             if try_num == max_retries:
                 log_func = logging.fatal
                 message = "Giving up! Could not read data from '%s': %s"
