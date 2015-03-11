c050d285d4919b3504711baba8be5b712417e090
Petar Marić <petar.maric@gmail.com>
2015-03-11 09:11:14 +0100

    Updated supported subtitle language markers as TED updated their HTML layout

--- metaTED/crawler/get_supported_subtitle_languages.py
+++ metaTED/crawler/get_supported_subtitle_languages.py
@@ -6,7 +6,7 @@ import re
 
 LANGUAGES_LIST_URL = 'http://www.ted.com/participate/translate/our-languages'
 _LANGUAGES_SELECTOR = CSSSelector('div.languages div div div a')
-_LANGUAGE_CODE_RE = re.compile('/talks/browse\?language=([\w\-]+)')
+_LANGUAGE_CODE_RE = re.compile('/talks\?language=([\w\-]+)')
 
 
 class NoSupportedSubtitleLanguagesFound(Exception):
